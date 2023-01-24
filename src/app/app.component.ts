529201f8181fec6c06215f436df896e54063c3d8
Icetune <109099573+Icetune@users.noreply.github.com>
2023-01-24 18:57:15 +0100

    todos abrufen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-todo';
+
+  todos$: Observable<any>;  //Observable ist eine Veriable, die sich updated
+  constructor(firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((newTodos) => {
+      console.log('Neue Todos sind:', newTodos)
+    });
+
+  }
+
 }
