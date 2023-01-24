12894f74f27da3224404b37c417bbf1687b89c53
Icetune <109099573+Icetune@users.noreply.github.com>
2023-01-24 18:05:06 +0100

    dude i think i made progress

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'fir-todo-c6f9e',
+    appId: '1:540560569305:web:ffde3ee3f690bdaa4ddfc8',
+    storageBucket: 'fir-todo-c6f9e.appspot.com',
+    apiKey: 'AIzaSyA067d0vXjRtSTlESqW9jsFlsH2tcGwzy8',
+    authDomain: 'fir-todo-c6f9e.firebaseapp.com',
+    messagingSenderId: '540560569305',
+  }
+};
